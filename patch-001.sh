8cd62eae404be607b9ef8355b9cd2bc2a88b7f0f
sabubenlycos <75975521+sabubenlycos@users.noreply.github.com>
2021-07-24 22:16:48 +0530

    Update patch-001.sh

--- patch-001.sh
+++ patch-001.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-sudo rm -rf /opt/watchy/ 
+sudo rm -rf /opt/watchy/bond007-ui
 wget https://github.com/benlycos/patch-watchy/raw/main/bond007-ui.tar.gz -O /tmp/bond007-ui.tar.gz
 tar -xvf  /tmp/bond007-ui.tar.gz -C /opt/watchy/
 sudo reboot

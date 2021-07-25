97851e3bd9a7c8c991f001af20fd8b907dcbc51d
sabubenlycos <75975521+sabubenlycos@users.noreply.github.com>
2021-07-25 10:30:22 +0530

    Update patch-001.sh

--- patch-001.sh
+++ patch-001.sh
@@ -3,4 +3,5 @@ sudo rm -rf /opt/watchy/bond007-ui
 wget https://github.com/benlycos/patch-watchy/raw/main/bond007-ui.tar.gz -O /tmp/bond007-ui.tar.gz
 tar -xvf  /tmp/bond007-ui.tar.gz -C /opt/watchy/
 #SLACK_URL=$(wget -q https://raw.githubusercontent.com/benlycos/automation-tests/main/tests/slack_url.gpg -O - | openssl aes-256-cbc -d -a -pass pass:somepassword)
+# curl -X POST --data-urlencode "payload={\"text\": \"Patch patch-001 addition for ${SLK_KEY} done. Device is going to get restarted\"}" ${SLACK_URL}
 sudo reboot

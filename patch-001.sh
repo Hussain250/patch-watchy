ac6b09a675efaaebf7f0254b281e6b2ee7cab223
sabubenlycos <75975521+sabubenlycos@users.noreply.github.com>
2021-07-25 10:27:23 +0530

    Update patch-001.sh

--- patch-001.sh
+++ patch-001.sh
@@ -2,4 +2,5 @@
 sudo rm -rf /opt/watchy/bond007-ui
 wget https://github.com/benlycos/patch-watchy/raw/main/bond007-ui.tar.gz -O /tmp/bond007-ui.tar.gz
 tar -xvf  /tmp/bond007-ui.tar.gz -C /opt/watchy/
+#SLACK_URL=$(wget -q https://raw.githubusercontent.com/benlycos/automation-tests/main/tests/slack_url.gpg -O - | openssl aes-256-cbc -d -a -pass pass:somepassword)
 sudo reboot

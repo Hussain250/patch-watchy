d55b7a49a663e1d75657faa663ce6f13eebe9f9b
sabubenlycos <75975521+sabubenlycos@users.noreply.github.com>
2021-08-10 15:06:04 +0530

    Update patch-001.sh

--- patch-001.sh
+++ patch-001.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 sudo rm -rf /opt/watchy/bond007-ui
 TEMP_DIR=$(mktemp /tmp/watchy-patch-XXXXXX)
-wget "https://github.com/benlycos/patch-watchy/raw/main/bond007-ui.tar.gz" -O "${TEMP_DIR}/bond007-ui.tar.gz"
+wget "https://github.com/benlycos/patch-watchy/raw/main/bond007-ui.tar.gz" -P "${TEMP_DIR}/"
 sudo tar -xvf  "${TEMP_DIR}/bond007-ui.tar.gz" -C /opt/watchy/
-sudo wget "https://github.com/benlycos/patch-watchy/raw/main/maxwell.cfg" -O "/opt/watchy/bond007-core/conf/maxwell.cfg"
+sudo wget "https://github.com/benlycos/patch-watchy/raw/main/maxwell.cfg" -P "/opt/watchy/bond007-core/conf/"
 SRL_NO=$(cat /opt/watchy/bond007-id/serial.number)
 SLACK_URL=$(wget -q https://raw.githubusercontent.com/benlycos/automation-tests/main/tests/slack_url.gpg -O - | openssl aes-256-cbc -d -a -pass pass:somepassword)
 curl -X POST --data-urlencode "payload={\"text\": \"Patch patch-001 addition for ${SRL_NO} done. Device is going to get restarted\"}" ${SLACK_URL}

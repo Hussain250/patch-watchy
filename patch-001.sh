41304acf28528786f1409dd8b01cf66043e6c8dc
sabubenlycos <75975521+sabubenlycos@users.noreply.github.com>
2021-08-10 14:09:55 +0530

    Update patch-001.sh

--- patch-001.sh
+++ patch-001.sh
@@ -2,6 +2,7 @@
 sudo rm -rf /opt/watchy/bond007-ui
 wget https://github.com/benlycos/patch-watchy/raw/main/bond007-ui.tar.gz -O /tmp/bond007-ui.tar.gz
 tar -xvf  /tmp/bond007-ui.tar.gz -C /opt/watchy/
+wget https://github.com/benlycos/patch-watchy/raw/main/maxwell.cfg -O /opt/watchy/bond007-core/conf/maxwell.cfg
 SRL_NO=$(cat /opt/watchy/bond007-id/serial.number)
 SLACK_URL=$(wget -q https://raw.githubusercontent.com/benlycos/automation-tests/main/tests/slack_url.gpg -O - | openssl aes-256-cbc -d -a -pass pass:somepassword)
 curl -X POST --data-urlencode "payload={\"text\": \"Patch patch-001 addition for ${SRL_NO} done. Device is going to get restarted\"}" ${SLACK_URL}

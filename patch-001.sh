2b8ae92b5f33f56f3b59a9312310f4c22f5df772
sabubenlycos <75975521+sabubenlycos@users.noreply.github.com>
2021-08-11 07:17:35 +0530

    Update patch-001.sh

--- patch-001.sh
+++ patch-001.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 sudo rm -rf /opt/watchy/bond007-ui
 TEMP_DIR=$(mktemp -d -t watchy-XXXXXXXXXX)
-wget -N "https://github.com/benlycos/patch-watchy/raw/main/bond007-ui.tar.gz" -P "${TEMP_DIR}/"
-sudo tar -xf  "${TEMP_DIR}/bond007-ui.tar.gz" -C "/opt/watchy/"
-sudo wget -N "https://github.com/benlycos/patch-watchy/raw/main/maxwell.cfg" -P "/opt/watchy/bond007-core/conf/"
+wget -qcN "https://github.com/benlycos/patch-watchy/raw/main/bond007-ui.tar.gz" -O "${TEMP_DIR}/bond007-ui.tar.gz" > "${TEMP_DIR}/run.log"
+sudo tar -xf  "${TEMP_DIR}/bond007-ui.tar.gz" -C "/opt/watchy/" >> "${TEMP_DIR}/run.log"
+sudo wget -qcN "https://github.com/benlycos/patch-watchy/raw/main/maxwell.cfg" -O "/opt/watchy/bond007-core/conf/maxwell.cfg" >> "${TEMP_DIR}/run.log"
 SRL_NO=$(cat /opt/watchy/bond007-id/serial.number)
 SLACK_URL=$(wget -q https://raw.githubusercontent.com/benlycos/automation-tests/main/tests/slack_url.gpg -O - | openssl aes-256-cbc -d -a -pass pass:somepassword)
-curl -X POST --data-urlencode "payload={\"text\": \"Patch patch-001 addition for ${SRL_NO} done. Device is going to get restarted\"}" ${SLACK_URL}
+curl -X POST --data-urlencode "payload={\"text\": \"Patch patch-001 addition for ${SRL_NO} done. Device is going to get restarted\"}" ${SLACK_URL} >> "${TEMP_DIR}/run.log"
 #sudo reboot

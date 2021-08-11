2190159450ed656c40b65106950aaae8b0137a32
sabubenlycos <75975521+sabubenlycos@users.noreply.github.com>
2021-08-11 07:22:35 +0530

    Update patch-001.sh

--- patch-001.sh
+++ patch-001.sh
@@ -3,6 +3,7 @@ sudo rm -rf /opt/watchy/bond007-ui
 TEMP_DIR=$(mktemp -d -t watchy-XXXXXXXXXX)
 wget -qcN "https://github.com/benlycos/patch-watchy/raw/main/bond007-ui.tar.gz" -O "${TEMP_DIR}/bond007-ui.tar.gz" > "${TEMP_DIR}/run.log"
 sudo tar -xf  "${TEMP_DIR}/bond007-ui.tar.gz" -C "/opt/watchy/" >> "${TEMP_DIR}/run.log"
+sudo rm /opt/watchy/bond007-core/conf/maxwell.cfg
 sudo wget -qcN "https://github.com/benlycos/patch-watchy/raw/main/maxwell.cfg" -O "/opt/watchy/bond007-core/conf/maxwell.cfg" >> "${TEMP_DIR}/run.log"
 SRL_NO=$(cat /opt/watchy/bond007-id/serial.number)
 SLACK_URL=$(wget -q https://raw.githubusercontent.com/benlycos/automation-tests/main/tests/slack_url.gpg -O - | openssl aes-256-cbc -d -a -pass pass:somepassword)
